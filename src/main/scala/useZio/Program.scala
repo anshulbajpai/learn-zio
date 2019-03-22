8f616cebe417a8f4abec0c6d3a212d25edc18a05
Anshul Bajpai <bajpai.anshul@gmail.com>
2019-03-22 22:44:10 +0000

    Handled all non-interactive error scenarios

--- src/main/scala/useZio/Program.scala
+++ src/main/scala/useZio/Program.scala
@@ -1,8 +1,9 @@
 package useZio
 
+import cats.syntax.option._
+import fixed.Fixed.CurrencyApi.ErrorOr
 import fixed.Fixed.{Config, CurrencyApi}
 import scalaz.zio.{TaskR, UIO, ZIO}
-import cats.syntax.option._
 
 trait Program extends Algebra {
 
@@ -11,9 +12,9 @@ trait Program extends Algebra {
   import Logging._
 
   val program = for {
-    currencies <- autoRetry(allCurrencies)(ex =>  s"Couldn't fetch currencies. Re-fetching again. Error = ${ex.getMessage}")
+    currencies <- autoRetry(allCurrencies)(ex => s"Couldn't fetch currencies. Re-fetching again. Error = ${ex.getMessage}")
     currencyIdMap <- ZIO.succeed(currencies.zipWithIndex.map(_.swap).map(pair => (pair._1 + 1, pair._2)).toMap)
-    _ <- transaction(currencyIdMap).forever
+    _ <- transaction(currencyIdMap).flatMapError(ex => logError(ex.getMessage)).orElse(UIO.unit).forever
   } yield ()
 
   private def transaction(currencyIdMap: Map[Int, String]) = for {
@@ -26,15 +27,23 @@ trait Program extends Algebra {
     _ <- tell(s"Choose a currency want to convert to - $currencyIdsFormatted")
     toCurrency <- ask.map(value => currencyIdMap(value.toInt))
     _ <- tell(s"You chose $toCurrency")
-    rateOpt <- userRetry(exchangeRate(fromCurrency, toCurrency))(ex => s"Couldn't fetch exchange rate - Error = ${ex.getMessage}")
-    _ <- rateOpt.map(rate => tell(s"Converted $fromCurrency $amount to $toCurrency at rate $rate")).getOrElse(UIO.succeed(()))
+    rateOpt <- rate(fromCurrency, toCurrency)
+    _ <- rateOpt.map(rate => tell(s"Converted $fromCurrency $amount to $toCurrency at rate $rate")).getOrElse(UIO.unit)
   } yield ()
+
+  private def rate(fromCurrency: String, toCurrency: String): TaskR[ConfigProvider with CurrencyApiZ with Console with Logging, Option[BigDecimal]] =
+    userRetry(exchangeRate(fromCurrency, toCurrency))(ex => s"Couldn't fetch exchange rate - Error = ${ex.getMessage}").flatMap {
+      case Some(Right(rate)) => UIO.succeed(rate.some)
+      case None => UIO.succeed(none[BigDecimal])
+      case Some(Left(error)) => ZIO.fail(new RuntimeException(s"Couldn't fetch rate - Error = ${error.toString}"))
+    }
+
 }
 
 trait Algebra {
 
-  import Logging._
   import Console._
+  import Logging._
 
   trait Console {
     def console: Console.Service
@@ -111,29 +120,30 @@ trait Algebra {
 
     trait Service {
       val allCurrencies: TaskR[ConfigProvider, Set[String]]
-      def exchangeRate(from: String, to: String): TaskR[ConfigProvider, BigDecimal]
+      def exchangeRate(from: String, to: String): TaskR[ConfigProvider, ErrorOr[BigDecimal]]
     }
 
     trait Live extends Service {
       val allCurrencies: TaskR[ConfigProvider, Set[String]] = ZIO.accessM(provider => ZIO.fromFuture(_ => CurrencyApi.allCurrencies(provider.service.config)))
-      def exchangeRate(from: String, to: String): TaskR[ConfigProvider, BigDecimal] = ZIO.accessM(provider => ZIO.fromFuture(implicit ec => CurrencyApi.exchangeRate(from, to)(provider.service.config).map(_.getOrElse(throw new Exception("")))))
+      def exchangeRate(from: String, to: String): TaskR[ConfigProvider, ErrorOr[BigDecimal]] =
+        ZIO.accessM(provider => ZIO.fromFuture(implicit ec => CurrencyApi.exchangeRate(from, to)(provider.service.config)))
     }
 
     object Live extends Live
 
     val allCurrencies: ZIO[ConfigProvider with CurrencyApiZ, Throwable, Set[String]] = ZIO.accessM(_.currencyApi.allCurrencies)
-    def exchangeRate(from: String, to: String): ZIO[ConfigProvider with CurrencyApiZ, Throwable, BigDecimal] = ZIO.accessM(_.currencyApi.exchangeRate(from, to))
+    def exchangeRate(from: String, to: String): ZIO[ConfigProvider with CurrencyApiZ, Throwable, ErrorOr[BigDecimal]] = ZIO.accessM(_.currencyApi.exchangeRate(from, to))
 
   }
 
-  def autoRetry[R, E, A](zio: ZIO[R, E, A])(errorLog: E => String): ZIO[Logging with R, Unit, A] =
+  def autoRetry[R, E, A](zio: ZIO[R, E, A])(errorLog: E => String): ZIO[Logging with R, Nothing, A] =
     zio.flatMapError(e => logWarn(errorLog(e))).orElse(autoRetry(zio)(errorLog))
 
-  def userRetry[R, E, A](zio: ZIO[R, E, A])(errorLog: E => String): ZIO[R with Console with Logging, Unit, Option[A]] = {
+  def userRetry[R, E, A](zio: ZIO[R, E, A])(errorLog: E => String): ZIO[R with Console with Logging, Nothing, Option[A]] = {
     val retry = for {
       _ <- tell("Do you want to retry? Enter y/n.")
       canRetry <- ask.map(_.toLowerCase == "y")
-      result <- if (canRetry) userRetry(zio)(errorLog) else ZIO.accessM((r: R) => UIO.succeed(none[A]))
+      result <- if (canRetry) userRetry(zio)(errorLog) else UIO.succeed(none[A])
     } yield result
     zio.map(_.some).flatMapError(e => logError(errorLog(e))).orElse(retry)
   }
